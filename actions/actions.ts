f5fa879805402b3b01fdf3f4a20eeebdb3954320
zidanhafiz <nadizsonata@gmail.com>
2024-08-06 16:30:17 +0700

    feat: Create open mystery box feature

--- actions/actions.ts
+++ actions/actions.ts
@@ -1,17 +1,20 @@
-"use server"
+"use server";
 import { HEADERS } from "@/utils/headers";
-import { doTransactions, getKeypairFromPrivateKey, PublicKey, sendSol } from "@/utils/solanaUtils";
+import {
+  doTransactions,
+  getKeypairFromPrivateKey,
+  PublicKey,
+  sendSol,
+} from "@/utils/solanaUtils";
 import { Keypair, Transaction } from "@solana/web3.js";
 import axios, { AxiosHeaderValue } from "axios";
 import moment from "moment";
 
-export const doTransaction = async (
-  data: { 
-    fromPrivateKey: string, 
-    toPublicKey: string, 
-    amount: string, 
-  }
-) => {
+export const doTransaction = async (data: {
+  fromPrivateKey: string;
+  toPublicKey: string;
+  amount: string;
+}) => {
   const fromKeypair = getKeypairFromPrivateKey(data.fromPrivateKey);
   const toPubKey = new PublicKey(data.toPublicKey);
 
@@ -22,30 +25,30 @@ export const doTransaction = async (
     console.log(message);
 
     return {
-      success: message 
-    }
+      success: message,
+    };
   } catch (error) {
     console.error(`Transaction failed: ${error}`);
     throw new Error(`transaction failed: ${error}`);
   }
-}
+};
 
 export const dailyLogin = async (token: AxiosHeaderValue, keypair: Keypair) => {
   try {
     const { data } = await axios({
-      url: 'https://odyssey-api-beta.sonic.game/user/check-in/transaction',
-      method: 'GET',
+      url: "https://odyssey-api-beta.sonic.game/user/check-in/transaction",
+      method: "GET",
       headers: { ...HEADERS, Authorization: token },
     });
 
-    const txBuffer = Buffer.from(data.data.hash, 'base64');
+    const txBuffer = Buffer.from(data.data.hash, "base64");
     const tx = Transaction.from(txBuffer);
     tx.partialSign(keypair);
     const signature = await doTransactions(tx, keypair);
 
     const response = await axios({
-      url: 'https://odyssey-api-beta.sonic.game/user/check-in',
-      method: 'POST',
+      url: "https://odyssey-api-beta.sonic.game/user/check-in",
+      method: "POST",
       headers: { ...HEADERS, Authorization: token },
       data: {
         hash: signature,
@@ -54,20 +57,64 @@ export const dailyLogin = async (token: AxiosHeaderValue, keypair: Keypair) => {
 
     return response.data;
   } catch (error: any) {
-    if (error.response.data.message === 'current account already checked in') {
+    if (error.response.data.message === "current account already checked in") {
       console.log(
-        `[ ${moment().format('HH:mm:ss')} ] Error in daily login: ${
+        `[ ${moment().format("HH:mm:ss")} ] Error in daily login: ${
           error.response.data.message
-        }`.red
+        }`.red,
       );
       return "Already claimed";
     } else {
       console.log(
-        `[ ${moment().format('HH:mm:ss')} ] Error claiming: ${
+        `[ ${moment().format("HH:mm:ss")} ] Error claiming: ${
           error.response.data.message
-        }`.red
+        }`.red,
       );
       return "Error claiming";
+    }
+  }
+};
+
+export const openMysteryBox = async (
+  token: AxiosHeaderValue,
+  keypair: Keypair,
+  retries: number = 3,
+): Promise<any> => {
+  try {
+    const { data } = await axios({
+      url: "https://odyssey-api-beta.sonic.game/user/rewards/mystery-box/build-tx",
+      method: "GET",
+      headers: { ...HEADERS, Authorization: token },
+    });
+
+    const txBuffer = Buffer.from(data.data.hash, "base64");
+    const tx = Transaction.from(txBuffer);
+    tx.partialSign(keypair);
+
+    const signature = await doTransactions(tx, keypair);
+
+    const response = await axios({
+      url: "https://odyssey-api-beta.sonic.game/user/rewards/mystery-box/open",
+      method: "POST",
+      headers: { ...HEADERS, Authorization: token },
+      data: {
+        hash: signature,
+      },
+    });
+
+    console.log(response.data);
+
+    return response.data;
+  } catch (error) {
+    if (retries > 0) {
+      console.log(
+        `Retrying opening mystery box... (${retries} retries left)`.yellow,
+      );
+      await new Promise((res) => setTimeout(res, 1000));
+      return openMysteryBox(token, keypair, retries - 1);
+    } else {
+      console.log(`Error opening mystery box: ${error}`.red);
+      throw error;
     }
   }
 }
--- app/open-box/page.tsx
+++ app/open-box/page.tsx
@@ -1,13 +1,125 @@
-import Link from "next/link"
+"use client";
+import { delay } from "@/utils/solanaUtils";
+import { zodResolver } from "@hookform/resolvers/zod";
+import axios from "axios";
+import Link from "next/link";
+import { useEffect, useRef, useState } from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+const validationSchema = z.object({
+  privateKey: z.string().min(10).trim(),
+  box: z.string(),
+});
+
+type ValidationSchema = z.infer<typeof validationSchema>;
 
 const OpenPage = () => {
-  return (
-    <div className='p-6 pt-10 max-w-xl mx-auto'>
-      <Link href='/' className="">〱 Back</Link>
-      <h1 className='text-2xl font-bold text-center mt-5'>Open Box</h1>
-      <p className="mt-5 text-center">Sorry this page is under construction.</p>
-    </div>
-  )
-}
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm<ValidationSchema>({
+    resolver: zodResolver(validationSchema),
+  });
+  const [logs, setLogs] = useState<string[]>([]);
+  const abortControllerRef = useRef<AbortController | null>(null);
 
-export default OpenPage
+  const onSubmit = async (data: ValidationSchema) => {
+    abortControllerRef.current = new AbortController();
+
+    for (let i = 0; i < parseInt(data.box); i++) {
+      try {
+        const res = await axios.post("/api/open", { privateKey: data.privateKey }, {
+          signal: abortControllerRef?.current.signal,
+        });
+
+        if (res.status !== 200) {
+          console.error(res.data.error);
+          setLogs((prev) => [...prev, res.data.error]);
+          return;
+        }
+
+        setLogs((prev) => [...prev, res.data.success]);
+        await delay(3 * 1000);
+      } catch (error: any) {
+        if (axios.isCancel(error)) {
+          console.log("Request canceled");
+          setLogs((prev) => [...prev, "Request canceled"]);
+        } else {
+          console.error(error);
+          setLogs((prev) => [...prev, error.message]);
+        }
+      }
+      break;
+    }
+    setLogs((prev) => [...prev, "All transactions is complete"]);
+  };
+
+  const handleAbort = () => {
+    abortControllerRef.current?.abort();
+  };
+
+  useEffect(() => {
+    return () => {
+      abortControllerRef.current?.abort();
+    };
+  }, []);
+
+  return (
+    <div className="p-6 pt-10 max-w-xl mx-auto">
+      <Link href="/" className="">
+        〱 Back
+      </Link>
+      <h1 className="text-2xl font-bold text-center mt-5">Open Box</h1>
+      <form onSubmit={handleSubmit(onSubmit)} className="mt-16 space-y-6">
+        <input
+          type="text"
+          placeholder="Your private key"
+          className="input input-bordered w-full"
+          disabled={isSubmitting}
+          {...register("privateKey")}
+        />
+        {errors.privateKey && (
+          <p className="text-red-400">Private key is required</p>
+        )}
+        <input
+          type="number"
+          placeholder="Box to open"
+          className="input input-bordered w-full"
+          disabled={isSubmitting}
+          {...register("box")}
+        />
+        {errors.box && <p className="text-red-400">Box is required</p>}
+        <button
+          type="submit"
+          className="btn btn-primary w-full mt-5"
+          disabled={isSubmitting}
+        >
+          Open
+        </button>
+      </form>
+      {isSubmitting && (
+        <button
+          className="btn btn-outline btn-error mt-5 w-full"
+          onClick={handleAbort}
+          disabled={!isSubmitting}
+        >
+          Stop
+        </button>
+      )}
+      <div className="mt-10 border-slate-600 border rounded-lg p-3 max-h-[200px] max-w-full overflow-scroll">
+        {logs.map((log, index) => (
+          <p
+            key={index}
+            className={`${log.includes("Success") || log.includes("complete") ? "text-green-400" : "text-red-400"} p-2 w-max`}
+          >
+            {log}
+          </p>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default OpenPage;
